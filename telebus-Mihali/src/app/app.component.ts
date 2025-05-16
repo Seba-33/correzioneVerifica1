5cf872947eefa8140e4cf3faad98a29169b94180
Seba-33 a <mihali.sebastian@iisgalvanimi.edu.it>
2025-05-16 14:07:01 +0000

    terzo commit

--- telebus-Mihali/src/app/app.component.ts
+++ telebus-Mihali/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { Post } from './post.model';
 import { CommonModule } from '@angular/common';
 import { PostComponent } from './post/post.component';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -13,14 +15,38 @@ import { PostComponent } from './post/post.component';
 })
 export class AppComponent {
   title = 'telebus-Mihali';
-  vettPost : Post[] = []
+  vettPost : Post[] = []  ;
+  obs : Observable<Post[]> ;
+  //mi faccio dare l'oggetto http da angular
+  constructor(private http: HttpClient){
+    //faccio la richiesta http, perche il costruttore parte all'avvio
+  this.obs =  this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts'); //richiedo i post dal mio server
+  //dico all'observable cosa fare quando ricevo i dati
+  this.obs.subscribe(this.getdata );
+  
+  }
 
-  addPost(nome : string, post : string) {
-    let nuovoPost = new Post(nome, post);     
-    
+  //la funzione che l'observable chiama quando riceve il data
+  getdata = (data : Post[]) => {
+    console.log(data);
+    this.vettPost = data; //assegno i dati ricevuti dall'array di post
+  }
+
+  obsPost! : Observable<any>;  //per ora non so che dati ricevero
+  addPost(userId : string, body : string) {
+    let nuovoPost = new Post(userId, body, "0","SENZA TITOLO");     
     this.vettPost.push(nuovoPost);
     console.log(this.vettPost);
-    
+
+    this.obsPost = this.http.post('https://jsonplaceholder.typicode.com/posts', nuovoPost)
+    this.obsPost.subscribe(this.rispostaPost);
+
 
   }
+
+  rispostaPost = (data : any) => {
+    console.log(data)
+  }
+
+  
 }
